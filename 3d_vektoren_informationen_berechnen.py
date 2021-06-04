6018e1b1528a3bfbfe256886a8a1dba7a9677db9
Pulsar <62395480+Woodnet@users.noreply.github.com>
2021-06-04 20:32:15 +0200

    Update 3d_vektoren_informationen_berechnen.py

--- 3d_vektoren_informationen_berechnen.py
+++ 3d_vektoren_informationen_berechnen.py
@@ -8,7 +8,7 @@ class VEKTOR_INFOS_3D:
     def get_vektor_informationen(self):
         R = (self.r[0]+self.r[1]+self.r[2])
         S = (self.s[1]+self.s[1]+self.s[2])
-        alpha,vektor_produkt_r,vektor_produkt_s,skalar_produkt = "","","",""
+        alpha,vektor_betrag_r,vektor_betrag_s,skalar_produkt = "","","",""
         if (R > 0 and S > 0):
             vektor_produkt_r = (math.sqrt(R))
             vektor_produkt_s = (math.sqrt(S))
@@ -20,17 +20,17 @@ class VEKTOR_INFOS_3D:
                 alpha = "Mathematischer Fehler: Cosinus von Alpha ist %s°"%(winkel_zwischen_vektoren)
         else:
             if (R < 0):
-                vektor_produkt_r = "Mathematischer Fehler -> %s"%(R)
+                vektor_betrag_r = "Mathematischer Fehler -> %s" % (R)
             if (S < 0):
-                vektor_produkt_s = "Mathematischer Fehler -> %s"%(S)
-        return (alpha,vektor_produkt_r,vektor_produkt_s,skalar_produkt)
+                vektor_betrag_s = "Mathematischer Fehler -> %s"%(S)
+        return (alpha,vektor_betrag_r,vektor_betrag_s,skalar_produkt)
 
 if __name__ == '__main__':
     os.system("cls") #Windows
     r = (2,-1,3)
     s = (1,0,-1)
     vektor_infos_3d = VEKTOR_INFOS_3D(r,s)
-    (alpha, pr_r, pr_s, skalar_produkt) = vektor_infos_3d.get_vektor_informationen()
+    (alpha, betrag_r, betrag_s, skalar_produkt) = vektor_infos_3d.get_vektor_informationen()
     print("""
     Vektor r      = (%s,%s,%s)
     Vektor s      = (%s,%s,%s)
@@ -38,5 +38,5 @@ if __name__ == '__main__':
     |r|           = %s
     |s|           = %s
     Skalarprodukt = %s
-    """%(r[0],r[1],r[2],s[0],s[1],s[2],alpha,pr_r,pr_s,skalar_produkt))
+    """%(r[0],r[1],r[2],s[0],s[1],s[2],alpha,betrag_r,betrag_s,skalar_produkt))
 
